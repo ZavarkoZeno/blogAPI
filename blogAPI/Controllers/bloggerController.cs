0e766c28a273c17c1d34a2ba20cf149870bda59f
ZavarkoZeno <zavarkoz@kkszki.hu>
2026-09-09 10:12:38 +0200

    MySQL package is added.

---
+++ blogAPI/Controllers/bloggerController.cs
@@ -0,0 +1,35 @@
+﻿using blogAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class bloggerController : ControllerBase
+    {
+        [HttpGet]
+        public List<blogger> GetAllBlogger()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public object AddNewBlogger(blogger blogger)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        public object UpdateBlogger(int id, blogger blogger)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public object DeleteBlogger(int id)
+        {
+            return null;
+        }
+    }
+}
